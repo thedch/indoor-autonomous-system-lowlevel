86b320441049b449555f178d34fd37437ed22354
Daniel Cornelius Hunter <dchunter@crm.ucsc.edu>
2018-04-20 18:02:47 -0700

    More PID functions added, remove example code

--- ians_controller/PID_velocity.cpp
+++ ians_controller/PID_velocity.cpp
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 #include "Motors.cpp"
 
-PID_velocity::PID_velocity(){
+PID_velocity::PID_velocity() {
   pid_error = 0;
   pid_target = 0;
   pid_motor = 0;
@@ -11,7 +11,7 @@ PID_velocity::PID_velocity(){
   pid_previous_error = 0;
   wheel_prev = 0;
   wheel_latest = 0;
-  something_then = millis();
+  then = millis();
   wheel_mult = 0;
   prev_encoder = 0;
 
@@ -29,20 +29,52 @@ PID_velocity::PID_velocity(){
   encoder_min = -32768;
   encoder_max = 32768;
   encoder_low_wrap = (encoder_max - encoder_min) * 0.3 + encoder_min;
-  pid_prev_vel = [0.0] * rolling_pts; /////////////////
+  std::deque<int> pid_prev_vel (8,0); 
+  // pid_prev_vel = [0.0] * rolling_pts;
   wheel_latest = 0.0;
 }
 
-void calc_velocity(){
+void PID_velocity::calc_velocity() {  
+  // check how much time has elapsed
+  float dt_duration = millis() - then; 
+  float dt = dt_duration * 1000;
 
+  // if no recent odometry data
+    // calc current velocity
+    // if below threshold
+      // set vel = 0
+    // else
+      // ??
+
+  // if we HAVE recent odom data
+  int cur_vel = (wheel_latest - wheel_prev) / dt
+  append_vel(cur_vel);
+  calc_rolling_vel();
+  wheel_prev = wheel_latest;
+  then = millis(); // update last_velocity_timestamp
+
+  // publish current vel?
   
+}
+
+void PID_velocity::append_vel(int val) { // Add velocity to history
+  pid_prev_vel.push_front(val);
+  pid_prev_vel.pop_back();  
+}
+
+void PID_velocity::calc_rolling_vel() {
+  int sum = 0
+  for (int i = 0; i < pid_prev_vel.size(); i++) {
+    sum += pid_prev_vel[i];
+  }
+  return float(sum/pid_prev_vel.size());
 }
 
 
 
-void do_pid(){
+void do_pid() {
   unsigned long pid_dt_duration = millis() - prev_pid_time; 
-  unsigned float pid_dt = pid_dt_duration * 1000;
+  unsigned float pid_dt = pid_dt_duration / 1000;
   prev_pid_time = millis();
 
   pid_error = pid_target - pid_vel;
@@ -52,7 +84,7 @@ void do_pid(){
 
   pid_motor = (pid_Kp * pid_error) + (pid_Ki * pid_intergral) + (pid_Kd * pid_derivative);
 
-  if(pid_motor > out_max){
+  if (pid_motor > out_max){
     pid_motor = out_max;
     pid_intergral = pid_intergral - (pid_error * pid_dt) //might need to be pid_dt 
   } else if(pid_motor < out_min){
--- ians_controller/PID_velocity.h
+++ ians_controller/PID_velocity.h
@@ -8,19 +8,48 @@
 #ifndef PID_VELOCITY_H
 #define PID_VELOCITY_H
 
-#include "Arduino.h"
-#include <ros.h>
+#include <deque>
+
 #include <std_msgs/Float32.h>
 #include <std_msgs/Int16.h>
+#include <ros.h>
+#include "Arduino.h"
 
 
 class PID_velocity
 {
   private:
-    unsigned long prev_pid_time;
     int pid_error;
-    int pid_target; 
-    
+    int pid_target;
+    int pid_motor;
+    int pid_vel;
+    int pid_intergral;
+    int pid_derivative;
+    int pid_previous_error;
+    wheel_prev;
+    wheel_latest;    
+    wheel_mult;
+    prev_encoder;
+    prev_pid_time;
+    int pid_Kp;
+    int pid_Ki;
+    int pid_Kd;
+    int out_min;
+    int out_max;
+    int rate;
+    int rolling_pts;
+    int timeout_ticks;
+    int ticks_per_meter;
+    velocity_threshold;
+    int encoder_min;
+    int encoder_max;
+    float then;
+    float dt_duration; // This is in milliseconds!
+    float dt; // This is in seconds!
+    encoder_low_wrap = (encoder_max - encoder_min) * 0.3 + encoder_min;
+    // pid_prev_vel = [0.0] * rolling_pts;
+    wheel_latest;
+      
   
   
   public:
